deb224a66cab232d42016c1c8b7ac388c1178d48
letsexisttogether <71719320+letsexisttogether@users.noreply.github.com>
2021-06-04 11:27:19 +0300

    Update 6 lab.cpp

--- 6 lab.cpp
+++ 6 lab.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include<string>
 #include<clocale>
 #include<iomanip>
@@ -7,11 +7,12 @@
 
 using namespace std;
 
+int check_int(string message);
 void cont();
 
 struct DATA
 {
-	int number = 0, age = 0, exp = 0, wage = 0;
+	int code = 0, suitCount = 0, weight = 0;
 	string surname;
 };
 struct NODE {
@@ -20,10 +21,9 @@ struct NODE {
 	NODE* right = nullptr;
 };
 
-class TREE
+class tree
 {
 private:
-	int count = 0;
 	NODE* root = nullptr;
 	void insert(NODE*& node, DATA& data)
 	{
@@ -33,21 +33,14 @@ private:
 			node->data = data;
 			node->left = nullptr;
 			node->right = nullptr;
-			count++;
 			return;
 		}
-		else if (data.number < node->data.number)
-		{
-			count++;
+		else if (data.surname < node->data.surname)
 			insert(node->left, data);
-		}
 		else
-		{
-			count++;
 			insert(node->right, data);
-		}
 	}
-	void insert_to_pour(NODE*& node, DATA& data)
+	void pInsert(NODE*& node, DATA& data)
 	{
 		if (node == nullptr)
 		{
@@ -55,64 +48,27 @@ private:
 			node->data = data;
 			node->left = nullptr;
 			node->right = nullptr;
-			count++;
 			return;
 		}
-		else if (data.exp < node->data.exp)
-			insert_to_pour(node->left, data);
+		else if (data.code < node->data.code)
+			pInsert(node->left, data);
 		else
-			insert(node->right, data);
+			pInsert(node->right, data);
 	}
 
 public:
-	int check_numbers(std::string message)
-	{
-		char test[10];
-		int numbers;
-		do
-		{
-			std::cout << message;
-			scanf_s("%d", &numbers);
-			fgets(test, sizeof(test), stdin);
-			if (numbers <= 0 || strlen(test) > 1)
-			{
-				fflush(stdin);
-				std::cout << "Entered invalide data!!!\nTry again!!!\n\n";
-			}
-		} while (numbers <= 0 || strlen(test) > 1);
-		return numbers;
-	}
 	void add(DATA& data)
 	{
 		insert(root, data);
 	}
-	void add_to_pour(DATA& data)
+	void pAdd(DATA& data)
 	{
-		insert_to_pour(root, data);
+		pInsert(root, data);
 	}
-	double middle_age(NODE* node = nullptr)
-	{
-		if (node == nullptr) node = root;
-		double sum = 0;
-		int count = 0;
-
-		if (node->left) {
-			sum += middle_age(node->left);
-			count++;
-		}
-		if (node->right) {
-			sum += middle_age(node->right);
-			count++;
-		}
-		count++;
-
-		return (sum + node->data.age) / (double)count;
-	}
-	void print(NODE* node = nullptr) {
+	void print_the_tree(NODE* node) {
 		if (node == nullptr)
 			node = root;
-		if (root == nullptr)
-		{
+		if (root == nullptr) {
 			cout << "Дерево пустое" << endl;
 			return;
 		}
@@ -120,16 +76,44 @@ public:
 		{
 			if (node->left)
 			{
-				cout << "+1 левый потомок" << endl;
-				print(node->left);
+				print_the_tree(node->left);
 			}
-			cout << "Личный номер: " << node->data.number << endl << "Фамилия: " << node->data.surname << endl << "Возраст: " << node->data.age << endl << "Стаж: " << node->data.exp << endl << "Зарплата" << node->data.wage << endl << endl;
+			cout << "Код билета: " << node->data.code << endl << "Фамилия: " << node->data.surname << endl << "Колличество багажа: " << node->data.suitCount << endl << "Вес багажа: " << node->data.weight << endl << endl;
 			if (node->right)
 			{
-				cout << "+1 Правый потомок" << endl;
-				print(node->right);
+				print_the_tree(node->right);
 			}
 		}
+	}
+	double middle_count(NODE* node) 
+	{
+		if (node == nullptr) 
+			node = root;
+		double sum = 0;
+		if (node->left) 
+		{
+			sum += middle_count(node->left);
+		}
+		if (node->right) 
+		{
+			sum += middle_count(node->right);
+		}
+		return double(sum + node->data.suitCount);
+	}
+	double middle_weight(NODE* node)
+	{
+		if (node == nullptr)
+			node = root;
+		double sum = 0;
+		if (node->left)
+		{
+			sum += middle_count(node->left);
+		}
+		if (node->right)
+		{
+			sum += middle_count(node->right);
+		}
+		return double(sum + node->data.weight);
 	}
 	void delete_tree(NODE* node = nullptr)
 	{
@@ -145,7 +129,7 @@ public:
 		if (node != root)
 			delete node;
 	}
-	void pour(TREE* Tree, NODE* node = nullptr)
+	void pour(tree* Tree, NODE* node = nullptr)
 	{
 		if (node == nullptr)
 			node = root;
@@ -153,56 +137,87 @@ public:
 			pour(Tree, node->left);
 		if (node->right)
 			pour(Tree, node->right);
-		Tree->add_to_pour(node->data);
+		Tree->pAdd(node->data);
 	}
-} tree;
+};
+
+int check_numbers(std::string message)
+{
+	char test[10];
+	int numbers;
+	do
+	{
+		std::cout << message;
+		scanf_s("%d", &numbers);
+		fgets(test, sizeof(test), stdin);
+		if (numbers <= 0 || strlen(test) > 1)
+		{
+			fflush(stdin);
+			std::cout << "Entered invalide data!!!\nTry again!!!\n\n";
+		}
+	} while (numbers <= 0 || strlen(test) > 1);
+	return numbers;
+}
+
 
 
 int main()
 {
-	DATA toRead;
-	TREE tree;
+	DATA d;
+	tree Tree;
 	setlocale(LC_ALL, "Russian");
-	int exit = 0;
-	char choice, take;
-	do
+	int size;
+	size = check_int("Введите колличество вводимых величин: ");
+	for (int i = 0; i < size; i++)
 	{
-		do
-		{
-			cout << "If you want enter data press 1, if you does not enter data press 2\n>>";
-			choice = _getch();
-			system("cls");
-		} while (choice != '1' && choice != '2');
-		if (choice == '1')
-		{
-			cout << "Enter surname of a worker:";
-			cin >> toRead.surname;
-			toRead.number = tree.check_numbers("Enter personal number of a worker: ");
-			toRead.age = tree.check_numbers("Enter age of a worker: ");
-			toRead.exp = tree.check_numbers("Enter experience of a worker: ");
-			toRead.wage = tree.check_numbers("Enter salary of a worker:  ");
-			tree.add(toRead);
-			cout << choice;
-		}
-		else if (choice == '2')
-		{
-			exit = 1;
-		}
-	} while (exit != 1);
-	cout << "First tree:" << endl;
-	tree.print();
-	cout << "\nAverage age: " << tree.middle_age() << endl;
-	tree.print();
-	TREE toPour;
-	tree.pour(&toPour);
-	tree.delete_tree();
-	tree.print();
-	cout << "\n\nSecond tree:" << endl;
-	toPour.print();
-	cout << "\n\nPress ESCAPE to exit\n" << endl;
-	do
-	{
-		take = _getch();
-	} while (take != 27);
+		system("cls");
+		cout << "Введите имя: ";
+		cin >> d.surname;
+		d.code = check_int("Введите код билета данного пассажира: ");
+		d.suitCount = check_int("Введите колличество багажа: ");
+		d.weight = check_int("Введите общий вес багажа: ");
+		Tree.add(d);
+	}
+
+	system("cls");
+	cout << "Первое дерево:" << endl;
+	Tree.print_the_tree(nullptr);
+	cout << "\nСреднее колличество багажа на человека: " << Tree.middle_count(nullptr)/size << endl;
+	cout << "\nСреднее вес багажа: " << Tree.middle_weight(nullptr) / size << endl;
+	cont();
+	system("cls");
+	tree toPour;
+	Tree.pour(&toPour);
+	Tree.delete_tree();
+	cout << "Первое дерево: " << endl;
+	Tree.print_the_tree(nullptr);
+	cout << "\n\nВторое дерево:" << endl;
+	toPour.print_the_tree(nullptr);
+	cont();
 	return 0;
+}
+
+int check_int(string message)
+{
+	int value;
+	char test[10];
+	do
+	{
+		cout << message;
+		scanf_s("%d", &value);
+		fgets(test, sizeof(test), stdin);
+		if (strlen(test) > 1 || value <= 0)
+		{
+			cout << "Ошибка ввода. Введите данные ещё раз!!!\n\n";
+			fflush(stdin);
+		}
+	} while (strlen(test) > 1 || value <= 0);
+	return value;
+}
+
+void cont()
+{
+	char taker;
+	cout << "\n\nДля продолжение нажмите Enter" << endl;
+	taker = _getch();
 }
